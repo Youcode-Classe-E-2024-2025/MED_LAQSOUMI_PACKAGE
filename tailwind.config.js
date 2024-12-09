d6a7bcbfc73616a5a1c7dafa89b625947d97e4f5
medlaq777 <m.laqsoumi@gmail.com>
2024-12-09 15:23:17 +0100

    Initial project setup with base HTML and CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/js/**/*.js", "./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        1000: "1000px",
+        820: "820px",
+        750: "750px",
+        680: "680px",
+        425: "425px",
+      },
+      fontFamily: {
+        Rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
