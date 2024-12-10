932acc299b77307c1b59ac04b14d2dd6c48dd270
medlaq777 <m.laqsoumi@gmail.com>
2024-12-10 10:18:08 +0100

    finish the V1 for the html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,7 @@
+// const toggleMenu = document.getElementById("toggleMenu");
+const dropdownMenu = document.getElementById("dropdownMenu");
 
-document.addEventListener("DOMContentLoaded", () => {
-    const sidebar = document.getElementById("sidebar");
-    const openSidebar = document.getElementById("openSidebar");
-    const closeSidebar = document.getElementById("closeSidebar");
-
-    openSidebar.addEventListener("click", () => {
-      sidebar.classList.remove("-translate-x-full");
-    });
-
-    closeSidebar.addEventListener("click", () => {
-      sidebar.classList.add("-translate-x-full");
-    });
-  });
-  
-  
-
-function openModal() {
-  document.getElementById('addModal').addEventListener('click', ()=> {
-    document.getElementById("modal").classList.remove('hidden')
-  })
-  
-}
+toggleMenu.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("hidden");
+  dropdownMenu.classList.toggle("-translate-y-full");
+});
