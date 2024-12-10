3b182dac74074077813a9d3aef3af22dcfbb0e7d
medlaq777 <m.laqsoumi@gmail.com>
2024-12-10 10:37:20 +0100

    finish the HTML AND TAILWIND and start js with make add btn dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-// const toggleMenu = document.getElementById("toggleMenu");
+const addBtn = document.getElementById("addModal");
+const closeModal = document.getElementById("closeModal");
+const cancelModal = document.getElementById("cancelModal");
 const dropdownMenu = document.getElementById("dropdownMenu");
+const modal = document.getElementById("modal");
 
 toggleMenu.addEventListener("click", () => {
   dropdownMenu.classList.toggle("hidden");
   dropdownMenu.classList.toggle("-translate-y-full");
 });
+
+addBtn.addEventListener('click', ()=> {
+    modal.classList.remove("hidden");
+})
+
+closeModal.addEventListener('click', ()=> {
+    modal.classList.add("hidden");
+})
+cancelModal.addEventListener('click', ()=> {
+    modal.classList.add("hidden");
+})
